f13cce47fd7608362e53c66ec96347d5438853cd
andreabertolini1995 <andre.berto95@gmail.com>
2023-04-28 13:37:18 +0200

    Initial commit

---
+++ philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abertoli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 17:33:54 by abertoli          #+#    #+#             */
+/*   Updated: 2022/10/21 18:29:38 by abertoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+# define FRACT_OL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+#endif
